ee40acadeaced3f9eea3f7c014b722af18b7e08e
Shadi Ahmed <79768622+ShadiAhmed1986@users.noreply.github.com>
2021-04-08 20:57:11 +0100

    Update TestFromFeatureBranch.java

--- src/librarymanagementsystem/TestFromFeatureBranch.java
+++ src/librarymanagementsystem/TestFromFeatureBranch.java
@@ -4,7 +4,7 @@ public class TestFromFeatureBranch {
 
 	public static void main(String[] args) {
 		System.out.println("This is a test from feature branch");
-		System.out.println("bunu bir arkadas deistirdi");
+		System.out.println("bunu bir arkadas tekrar deistirdi deistirdi");
 	}
 
 }
